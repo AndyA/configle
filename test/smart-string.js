51d4fcc22dcdb6654aadb51131649e4f10873b0d
Andy Armstrong <andy@hexten.net>
2015-06-25 17:40:38 +0100

    Smart strings.

--- test/smart-string.js
+++ test/smart-string.js
@@ -8,35 +8,82 @@ var SmartString = require("../lib/smart-string.js");
 
 describe("SmartString", function() {
 
-  it("should smarten a relative path", function() {
-    var smartStr = SmartString.smarten("mystuff", "/tmp");
-    expect(smartStr)
-      .to.be.instanceOf(String);
-    expect(smartStr)
-      .to.respondTo("getPath");
-    expect(smartStr.getPath())
-      .to.equal("/tmp/mystuff");
+  describe("smarten", function() {
+
+    it("should smarten a relative path", function() {
+      var smartStr = SmartString.smarten("mystuff", "/tmp");
+      expect(smartStr)
+        .to.be.instanceOf(String);
+      expect(smartStr)
+        .to.respondTo("toPathName");
+      expect(smartStr.toPathName())
+        .to.equal("/tmp/mystuff");
+    });
+
+    it("should smarten an absolute path", function() {
+      var smartStr = SmartString.smarten("/usr/bin/node", "/tmp");
+      expect(smartStr)
+        .to.be.instanceOf(String);
+      expect(smartStr)
+        .to.respondTo("toPathName");
+      expect(smartStr.toPathName())
+        .to.equal("/usr/bin/node");
+    });
+
+    it("should pass through a smartened string", function() {
+      var smartStr = SmartString.smarten("mystuff", "/tmp");
+      var anotherStr = SmartString.smarten(smartStr, "/opt");
+      expect(anotherStr)
+        .to.be.instanceOf(String);
+      expect(anotherStr)
+        .to.respondTo("toPathName");
+      expect(anotherStr.toPathName())
+        .to.equal("/tmp/mystuff");
+    });
+
   });
 
-  it("should smarten an absolute path", function() {
-    var smartStr = SmartString.smarten("/usr/bin/node", "/tmp");
-    expect(smartStr)
-      .to.be.instanceOf(String);
-    expect(smartStr)
-      .to.respondTo("getPath");
-    expect(smartStr.getPath())
-      .to.equal("/usr/bin/node");
+  describe("staySmart", function() {
+
+    function filt(str) {
+      return "foo";
+    }
+
+    it("should not smarten a non-smart string", function() {
+      var str = "Hello, World\n";
+      var filtStr = SmartString.staySmart(str, filt);
+      expect(SmartString.isSmart(filtStr))
+        .to.be.false;
+    });
+
+    it("should retain the smarts of a smart string", function() {
+      var smartStr = SmartString.smarten("${ENV}", "/tmp");
+      var filtStr = SmartString.staySmart(smartStr, filt);
+      expect(SmartString.isSmart(filtStr))
+        .to.be.true;
+      expect(filtStr.toPathName())
+        .to.equal("/tmp/foo");
+    });
   });
 
-  it("should pass through a smartened string", function() {
-    var smartStr = SmartString.smarten("mystuff", "/tmp");
-    var anotherStr = SmartString.smarten(smartStr, "/opt");
-    expect(anotherStr)
-      .to.be.instanceOf(String);
-    expect(anotherStr)
-      .to.respondTo("getPath");
-    expect(anotherStr.getPath())
-      .to.equal("/tmp/mystuff");
+  describe("setString", function() {
+
+    it("should not smarten a non-smart string", function() {
+      var str = "Hello, World\n";
+      var filtStr = SmartString.setString(str, "foo");
+      expect(SmartString.isSmart(filtStr))
+        .to.be.false;
+    });
+
+    it("should retain the smarts of a smart string", function() {
+      var smartStr = SmartString.smarten("${ENV}", "/tmp");
+      var filtStr = SmartString.setString(smartStr, "foo");
+      expect(SmartString.isSmart(filtStr))
+        .to.be.true;
+      expect(filtStr.toPathName())
+        .to.equal("/tmp/foo");
+    });
+
   });
 
 });
